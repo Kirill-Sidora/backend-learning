01bbddc50cdaffe54ab21c9405b24aca60cfc230
Kirill Sidora <kirillsidora@yandex.ru>
2026-01-15 17:43:19 +0300

    Some code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { Sequelize } from "sequelize";
+import { datavaseConfig } from "./utils/configs";
 
 const application = express();
 
@@ -9,6 +11,34 @@ application.get("/", (_, response) => {
 });
 
 const PORT = Number(process.env.PORT);
+
+const sequelize = new Sequelize(
+    datavaseConfig.database,
+    datavaseConfig.username,
+    datavaseConfig.password,
+    {
+        host: datavaseConfig.host,
+        port: datavaseConfig.port,
+        dialect: datavaseConfig.dialect,
+        logging: datavaseConfig.logging,
+        define: {
+            underscored: true,
+        },
+    }
+);
+
+const foo = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log(
+            "Connection to the database has been established successfully."
+        );
+    } catch (error) {
+        console.log("Unable to connect to the database:", error);
+    }
+};
+
+foo();
 
 application.listen(PORT, () => {
     console.log(`🚀 Express server is listening on http://localhost:${PORT}`);
