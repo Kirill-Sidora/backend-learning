277a312e96566a0a9780c314c2b03ba11fc7d6eb
Kirill Sidora <kirillsidora@yandex.ru>
2026-01-19 17:30:26 +0300

    feat: Created swagger docs

--- src/utils/configs/swagger.ts
+++ src/utils/configs/swagger.ts
@@ -2,16 +2,49 @@ import { applicationConfig } from "./application";
 import { Paths } from "./../../utils/constants/app";
 import { Options } from "swagger-jsdoc";
 
-const successCreateCarEntitySchema = {
+const successResponseSchema = {
     type: "object",
     properties: {
-        brand: { type: "string", example: "BMW" },
-        model: { type: "string", example: "X6" },
-        year_of_release: { type: "number", example: 2005 },
-        cost: { type: "number", example: 1999999 },
+        ok: {
+            type: "boolean",
+            example: true,
+        },
+        message: {
+            type: "string",
+            example: "Request completed successfully",
+        },
+        meta: {
+            oneOf: [
+                { type: "object" },
+                { type: "array", items: {} },
+                { type: null },
+            ],
+        },
+        tockens: {
+            type: "object",
+            descrition: "JWT payload or empty object",
+        },
     },
-    require: ["brand", "model", "year_of_release", "cost"],
+    required: ["ok", "message", "meta", "tockens"],
 };
+
+const errorResponseSchema = {
+    type: 'object',
+    properties: {
+        ok: {
+            type: 'boolean', exmple: 'false',
+        },
+        error: {
+            type: 'object',
+            properties: {
+                code: { type: 'string', example: "UNAUTHORIZED" },
+                message: { type: 'string', example: "Unauthorized" },
+            },
+            required: [ 'code', 'message' ],
+        },
+    },
+    required: [ 'ok', 'error' ],
+}
 
 const definition = {
     openapi: "3.0.3",
@@ -33,31 +66,142 @@ const definition = {
     ],
     components: {
         schemas: {
+            SuccessResponse: successResponseSchema,
+            ErrorResponse: errorResponseSchema,
             CarEntity: {
-                type: 'object',
+                type: "object",
                 properties: {
-                    carId: { type: 'string', format: 'uuid' },
-                    brand: { type: 'string' },
-                    model: { type: 'string' },
-                    year_of_release: { type: 'number' },
-                    cost: { type: 'number' },
-                    createdAt: { type: 'string', format: 'date-time' },
-                    updatedAt: { type: 'string', format: 'date-time' },
+                    carId: { type: "string", format: "uuid" },
+                    brand: { type: "string" },
+                    model: { type: "string" },
+                    year_of_release: { type: "number" },
+                    cost: { type: "number" },
+                    createdAt: { type: "string", format: "date-time" },
+                    updatedAt: { type: "string", format: "date-time" },
                 },
                 required: [
-                    'carId',
-                    'brand',
-                    'model',
-                    'year_of_release',
-                    'cost',
-                    'createdAt',
-                    'updatedAt',
+                    "carId",
+                    "brand",
+                    "model",
+                    "year_of_release",
+                    "cost",
+                    "createdAt",
+                    "updatedAt",
                 ],
             },
             CarCreateRequest: {
-                 
-            }
-        }
+                type: "object",
+                properties: {
+                    brand: {
+                        type: "string",
+                        example: "BMW",
+                    },
+                    model: {
+                        type: "string",
+                        example: "X6",
+                    },
+                    year_of_release: {
+                        type: "number",
+                        example: "2007",
+                    },
+                    cost: {
+                        type: "number",
+                        example: "1999999",
+                    },
+                },
+                required: ["brand", "model", "year_of_release", "cost"],
+            },
+            CarUpdateRequest: {
+                type: 'object',
+                properties: {
+                    brand: {
+                        type: 'string',
+                    },
+                    model: {
+                        type: 'string',
+                    },
+                    year_of_release: {
+                        type: 'number',
+                    },
+                    cost: {
+                        type: 'number',
+                    },
+                },
+            },
+            CarCreateResponse: {
+                allOf: [
+                    { $ref: "#/components/schemas/SuccessResponse" },
+                    {
+                        properties: {
+                            meta: {
+                                $ref: "#/components/schemas/CarEntity",
+                            },
+                        },
+                    },
+                ],
+            },
+            CarRequestEntity: {
+                type: "object",
+                properties: {
+                    carId: {
+                        type: "string",
+                        format: "uuid",
+                    },
+                    brand: {
+                        type: "string",
+                    },
+                    model: {
+                        type: "string",
+                    },
+                    year_of_release: {
+                        type: "number",
+                    },
+                    cost: {
+                        type: "number",
+                    },
+                },
+                required: [
+                    "carId",
+                    "brand",
+                    "model",
+                    "year_of_release",
+                    "cost",
+                ],
+            },
+            CarResponse: {
+                allOf: [
+                    { $ref: '#/components/schemas/SuccessResponse' },
+                    {
+                        properties: {
+                            meta: {
+                                type: 'array',
+                                items: { $ref: '#/components/schemas/CarEntity'},
+                            },
+                        },
+                    },
+                ],
+            },
+            // CarRequestCreateResponse: {
+            //     allOf: [
+            //         { $ref: '#/components/schemas/SuccessResponse' },
+            //         { 
+            //             properties: {
+            //                 meta: {
+            //                     oneOf: [
+            //                         { $ref: '#components/schemas/CarRequestEntity' },
+            //                         {
+            //                             type: 'object',
+            //                             properties: {
+            //                                 joined: { type: 'boolean' },
+            //                             },
+            //                         },
+            //                     ],
+            //                 },
+            //             },
+            //         },
+            //     ],
+            // },
+        },
     },
     externalDocs: {
         description: `Swagger UI exponsed under ${Paths.DOCS_PATH}`,
